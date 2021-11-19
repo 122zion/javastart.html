69dfaacfa54d05889a843323c6b66358854eb190
122zion <56268884+122zion@users.noreply.github.com>
2021-11-18 23:44:28 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,94 @@
+/* let age = 25;
+let year= 2019;
+
+console.log(age, year);
+
+age = 30; 
+console.log(age);
+
+const points = 100;
+console.log(points);
+
+var score = 74;
+console.log(score);
+
+strings
+console.log('hello,world');
+let email = 'mario@thenetninja.co.uk';
+console.log(email);
+
+string concatenation
+let fullName = firstName + '' + lastName;
+console.log(fullName);
+
+getting characters
+console.log(fullName[2]);
+
+string length
+console.log(fullName.length);
+
+strong methods
+console.log(fullName.toUpperCase());
+let result = fullName.toLowerCase();
+console.log(result, fullName); 
+
+common string methods
+let email = 'mario@thenetninja.co.uk';
+
+let result = email.slice(2,5);
+
+let radius = 10;
+const pi = 3.14; 
+
+console.log(radius, pi)
+math operators +, -, *, /, **, %
+
+console.log(10/2);
+let result = radius % 3;
+let result = pi * radius**2;
+
+order of operations - B I D M A S
+console.log(result);
+
+let likes = 10;
+
+for (let i=0; i< 5; i++){
+console.log('in loop:'. i);
+}
+
+console.log('loop finished'); 
+
+const age = 25; 
+if(age > 20){
+console.log('you are over 20 years old')
+}
+
+const ninjas = ['shaun','ryu', 'chun-li', 'yoshi'];
+
+if(ninjas.length>4){
+console.log("that's alot of ninjas")
+} */
+
+/* const speak = function(name){
+console.log('good day');
+};
+
+greet();
+greet();
+greet();
+
+function greet(,name ){
+console.log('hello there');
+// } */
+
+// let user = {
+//   name: 'crystal',
+//   age: 30,
+//   email:'school@email.com
+//   location: 'berlin',
+//   blogs: [How to braid a dog]
+//   };
+
+//   console.log(user.name);
+//   console.log(user.age);
+//   console.log(user.email);
